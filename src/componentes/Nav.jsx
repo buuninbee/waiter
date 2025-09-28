6b346f3d3f6b21b3603858dc1fc298a8eaf440aa
buuninbee <buunin.bee@gmail.com>
2025-09-28 00:00:35 -0300

    feat: corrigindo bug de renderização do menu

--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -1,37 +1,43 @@
 import Logo from "../assets/waiter.svg"
-import Menufechado from "../assets/menu-fechado.svg"
+import MenuAberto from "../assets/menu-aberto.svg"
+import MenuFechado from "../assets/menu-fechado.svg"
 import "../index.css"
 import React from "react"
 
 const Nav = () => {
-
-  const [aberto, setAberto] = React.useState(false)
+  const [aberto, setAberto] = React.useState(() => {
+    const valorSalvo = localStorage.getItem("menuAberto")
+    return valorSalvo === "true"
+  })
+  
+  const alternarMenu = () => {
+    const novoEstado = !aberto
+    setAberto(novoEstado)
+    localStorage.setItem("menuAberto", novoEstado)
+  }
+  
   return (
-    <header className="bg-[var(--primario-700)] px-8 py-7 flex justify-between">
+    <header className={aberto ? "bg-[var(--primario-700)] px-8 py-7 flex justify-between" : "bg-[var(--primario-700)] h-dvh w-full fixed px-8 py-7 flex justify-between"}>
     <nav className="">
-      <div className="">
+      <div className={aberto ? 'block' : 'hidden'}>
         <a href="">
           <img src={Logo} alt="logo waiter"/>
         </a>
       </div>
 
-      <div className="hidden">
-        <ul className="">
+      <div className={aberto ? 'hidden' : 'grid'}>
+        <ul className="grid gap-2 pt-10 text-2xl text-[var(--terciario-50)]">
           <li className=""><a href="">Home</a></li>
           <li className=""><a href="">Qr-code</a></li>
           <li className=""><a href="">Cardápio web</a></li>
         </ul>
-
-        <a className="" onclick="">
-          <img src="./assets/menu-aberto.svg" alt="botão pra abrir o menu"/>
-        </a>
       </div>
     </nav>
 
-    <div className="">
+    <div>
       <div>
-        <button className={aberto ? "bg-amber-200" : aberto} onClick={() => setAberto(!aberto)} >
-          <img className="w-6" src={Menufechado} alt="fechar menu"/>
+        <button onClick={alternarMenu} >
+          <img className="w-6 " src={aberto ? MenuAberto : MenuFechado} alt="icone do menu"/>
         </button>
       </div>
 
