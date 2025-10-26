c298331ab3387ad1c238984566a5f6be321c0319
buuninbee <buunin.bee@gmail.com>
2025-10-26 17:18:36 -0300

    fix: corrigindo a logica da navegação

--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -16,29 +16,28 @@ const Nav = () => {
     setAberto(novoEstado)
     localStorage.setItem("menuAberto", novoEstado)
   }
-  
+  const styleNav = "grid justify-between items-center-safe w-full xl:px-24"
+
   return (
-      <header className={aberto ? "bg-[var(--primario-700)] px-8 py-7 flex justify-between" : "bg-[var(--primario-700)] h-dvh w-full fixed px-8 py-7 flex justify-between"}>
-        <nav className="flex justify-between items-center-safe w-full xl:px-24">
+      <header className={aberto ? "bg-[var(--primario-700)] px-8 py-5 flex justify-between" : "bg-[var(--primario-700)] h-auto rounded-bl-4xl w-full fixed px-8 py-7 flex justify-between"}>
+        <nav className={aberto ? `${styleNav}  grid-cols-2` : `${styleNav} grid-cols-4`}>
           <div className={aberto ? 'block' : 'hidden'}>
             <NavLink to="/">
               <img className="w-36 h-auto" src={Logo} alt="logo waiter"/>
             </NavLink>
           </div>
 
-          <div className={aberto ? 'hidden' : 'grid'}>
-            <ul className="grid gap-2 pt-10 text-2xl text-[var(--terciario-50)]">
+          <div className={aberto ? 'hidden transition duration-800 ease-in-out' : 'grid self-start col-span-3 '}>
+            <ul className="grid gap-6 pt-10 text-2xl text-[var(--terciario-50)]">
               <li className=""><NavLink to="/">Home</NavLink></li>
               <li className=""><NavLink to="/qrCode">Qr-code</NavLink></li>
-              <li className=""><NavLink to="/cardapioDigital">Cardápio digital</NavLink></li>
+              <li className="w-full"><NavLink to="/cardapioDigital">Cardápio digital</NavLink></li>
             </ul>
           </div>
-          <div>
-            <div className="md:hidden">
-              <button onClick={alternarMenu} >
+          <div className={aberto ? 'grid justify-self-end' : 'grid justify-self-end self-start'}>
+              <button className="self-start block md:hidden" onClick={alternarMenu} >
                 <img className="w-7" src={aberto ? MenuAberto : MenuFechado} alt="icone do menu"/>
               </button>
-            </div>
 
             <ul className="hidden md:flex gap-7 text-xl text-[var(--terciario-50)]">
               <li className="link-paginas"><NavLink to="/">Home</NavLink></li>
