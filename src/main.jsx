b8fac6576995ea03d2d84e97edd41289b539e2f8
buuninbee <buunin.bee@gmail.com>
2025-10-26 18:28:42 -0300

    feat: adicionando redirecionamento das páginas de cadastro e login

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import Footer from './componentes/Footer.jsx'
 
 import CardapioDigital from "../src/pages/cardapioDigital.jsx"
 import QrCode from "../src/pages/qrCode.jsx"
+import Login from './login/Login.jsx'
+import Cadastro from './login/Cadastro.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,6 +19,8 @@ createRoot(document.getElementById('root')).render(
         <Route path='/'  element={<App />}/>
           <Route path='/cardapioDigital' element={<CardapioDigital/>}/>
           <Route path='/qrCode' element={<QrCode/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/cadastro' element={<Cadastro/>}/>
         </Routes>
       <Footer/>
     </BrowserRouter>
