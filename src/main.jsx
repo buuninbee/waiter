e1881887f0b894ed603de1a135160b7e77dfe348
buuninbee <buunin.bee@gmail.com>
2025-11-01 21:24:08 -0300

    feat: Alterando o roteamento da pagina

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Nav from './componentes/Nav.jsx'
-import Footer from './componentes/Footer.jsx'
 
 import CardapioDigital from "../src/pages/cardapioDigital.jsx"
 import QrCode from "../src/pages/qrCode.jsx"
 import Login from './login/Login.jsx'
 import Cadastro from './login/Cadastro.jsx'
+import Home from './pages/Home.jsx'
+
+import LayoutForm from './login/LayoutForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Nav/>
         <Routes>
-        <Route path='/'  element={<App />}/>
-          <Route path='/cardapioDigital' element={<CardapioDigital/>}/>
-          <Route path='/qrCode' element={<QrCode/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/cadastro' element={<Cadastro/>}/>
+        <Route element={<App />}>
+            <Route path='/' element={<Home/>}/>
+            <Route path='cardapioDigital' element={<CardapioDigital/>}/>
+            <Route path='qrCode' element={<QrCode/>}/>
+        </Route>  
+
+          <Route element={<LayoutForm/>} >
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/cadastro' element={<Cadastro/>}/>
+          </Route>
         </Routes>
-      <Footer/>
     </BrowserRouter>
   </StrictMode>,
 )
