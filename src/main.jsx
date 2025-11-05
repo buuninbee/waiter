3abed663aa3e3908b01e51b8c10ba1624b7d931a
buuninbee <buunin.bee@gmail.com>
2025-11-05 11:16:32 -0300

    add: instalando a biblioteca de componentes muiX

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import CardapioDigital from "../src/pages/cardapioDigital.jsx"
-import QrCode from "../src/pages/qrCode.jsx"
-import Login from './login/Login.jsx'
-import Cadastro from './login/Cadastro.jsx'
+import CardapioDigital from "./pages/CardapioDigital.jsx"
+import QrCode from "./pages/QrCode.jsx"
+import Login from './pages/login/Login.jsx'
+import Cadastro from './pages/login/Cadastro.jsx'
 import Home from './pages/Home.jsx'
 
-import LayoutForm from './login/LayoutForm.jsx'
+import LayoutForm from './pages/login/LayoutForm.jsx'
+import Pedidos from './pages/adm/Pedidos'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -26,6 +27,10 @@ createRoot(document.getElementById('root')).render(
             <Route path='/login' element={<Login/>}/>
             <Route path='/cadastro' element={<Cadastro/>}/>
           </Route>
+
+        <Route element={<Pedidos/>}>
+          <Route path='/adm/pedidos' element={<Pedidos/>}/>
+        </Route>
         </Routes>
     </BrowserRouter>
   </StrictMode>,
