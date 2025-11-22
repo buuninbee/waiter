394b9ad237257c38e9035f8a853e76d5b1f77ef8
buuninbee <buunin.bee@gmail.com>
2025-11-22 19:22:47 -0300

    feat: alterando a cor do botão secundario

--- src/componentes/Botao.jsx
+++ src/componentes/Botao.jsx
@@ -5,7 +5,7 @@ const defaultButtonClasses = "py-2 px-4 text-lg rounded-sm md:text-xl";
 
 const variants = {
   'primario': "bg-(--primario-700) text-(--terciario-400) cursor-pointer hover:bg-(--primario-900)",
-  'secundario': "bg-(--terciario-400) text-(--primario-700) border border-(--primario700) hover:bg-(--terciario-200) hover:text-(--primario-500)"
+  'secundario': "bg-(--terciario-100) text-(--primario-700) border border-(--primario700) hover:bg-(--terciario-200) hover:text-(--primario-500)"
 };
 
 
@@ -14,7 +14,7 @@ const Botao = ({text, path, variant = "primario", img}) => {
     <button className={cn(defaultButtonClasses, variants[variant])}>
         {
             img ? 
-            <Link to={path}>
+            <Link className="flex justify-center gap-2" to={path}>
                 <img src={img} alt={text} />
             {text}
             </Link> : 
