4317ebe2e77e40eb6bcd65bcdc25774eec7fd6ac
buuninbee <buunin.bee@gmail.com>
2025-11-24 01:05:15 -0300

    feat: adicionando o accordion de produtos da categorias

--- src/componentes/AcordianCategoria.jsx
+++ src/componentes/AcordianCategoria.jsx
@@ -6,8 +6,7 @@ import {
 } from "@/components/ui/accordion"
   
 import DialogoEditarProduto from "./DialogoEditarProduto"
-function AccordionDemo() {
-
+function ProdutosCategoria() {
     // const [open, setOpen] = React.useState(false);
 
     return (
@@ -18,12 +17,26 @@ function AccordionDemo() {
         defaultValue="item-1"
       >
         <AccordionItem value="item-1">
-          <AccordionTrigger>Ver itens</AccordionTrigger>
-          <AccordionContent className='relative'>
-            <div>
-                <h3>Coca-cola de 600ml</h3>
+          <AccordionTrigger>
+            Ver itens
+            </AccordionTrigger>
+          <AccordionContent className='relative grid gap-2'>
+            <div className=" border border-(--primario-800) py-5 px-3 rounded-lg">
+                <h3 className="text-xl">Coca-cola de 600ml</h3>
                 <p>Descrição: </p>
-                <p>R$ 12,00</p>
+                <p className="text-lg font-bold">R$ 12,00</p>
+                <DialogoEditarProduto />
+            </div>
+            <div className=" border border-(--primario-800) py-5 px-3 rounded-lg">
+                <h3 className="text-xl">Coca-cola de 200ml</h3>
+                <p>Descrição: </p>
+                <p className="text-lg font-bold">R$ 5,00</p>
+                <DialogoEditarProduto />
+            </div>
+             <div className=" border border-(--primario-800) py-5 px-3 rounded-lg">
+                <h3 className="text-xl">Coca-cola de 1L</h3>
+                <p>Descrição: </p>
+                <p className="text-lg font-bold">R$ 8,00</p>
                 <DialogoEditarProduto />
             </div>
           </AccordionContent>
@@ -32,7 +45,7 @@ function AccordionDemo() {
     )
 }
 
-export default AccordionDemo
+export default ProdutosCategoria
 
 
   
