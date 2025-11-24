dfccae68ab2794daaf9d44f348b8f9cf80253ca3
buuninbee <buunin.bee@gmail.com>
2025-11-24 10:26:15 -0300

    feat: centralizando a lista de produtos da pagina de cardápio

--- src/pages/adm/Cardapio.jsx
+++ src/pages/adm/Cardapio.jsx
@@ -7,10 +7,10 @@ import ProdutosCategoria from "@/componentes/AcordianCategoria"
 
 const Cardapio = () => {
   return (
-    <main className="grid pb-8 w-full md:pl-10 md:pb-0">
+    <main className="grid pb-8 md:pl-10 md:pb-0 md:w-[1200px] md:ml-auto md:mr-auto">
         <section className="grid gap-3.5 px-8 py-6 md:py-7 lg:pt-11">
             <div className="grid gap-7">
-                <button className="bg-(--primario-700) text-(--terciario-400) gap-3 py-3 text-lg px-1.5 flex justify-center rounded-xl">
+                <button className="bg-(--primario-700) text-(--terciario-400) gap-3 py-3 text-lg px-1.5 flex justify-center rounded-xl cursor-pointer">
                     <img className="w-6" src={addCategoria} alt="" />
                     <p>Adicionar categoria</p>
                 </button>
@@ -21,35 +21,46 @@ const Cardapio = () => {
                 </div>
             </div>
         </section>
-        <section className="grid gap-3.5 px-8 py-6 md:py-7 lg:pt-11">
+        <section className="grid gap-3.5 px-8 py-6 md:py-7 lg:pt-11 md:gap-5">
             <div className="grid border border-(--primario-600) py-2 px-3 rounded-lg">
                 <div className="flex justify-between pb-1.5">
                     <h2 className="text-2xl">Bebidas</h2>
                     <button><img src={config} alt="" /></button>
                 </div>
-                {/* <div className="">
-                    <div className="flex justify-between text-(--primario-800)">
-                        <img className="w-32 rounded-lg" src="https://tse4.mm.bing.net/th/id/OIP.HLWGfV7i1irY6sqRMzfrPAHaHa?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3" alt="" srcset="" />
-                        <div className="grid">
-                            <h3 className="text-lg">Coca-cola de 600ml</h3>
-                            <div className="flex text-sm">
-                                <button >Preço: R$ 18,00</button>
-                                <button className="flex align-middle gap-1 ">
-                                    <img className="w-6 h-auto" src={editar} alt="" />
-                                    <p>Editar</p>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <button>Ver adicionais</button>
-                    </div>
-                </div> */}
-        <ProdutosCategoria />
 
-                <button className="bg-(--primario-700) text-(--terciario-400) gap-3 py-3 text-lg px-1.5 flex justify-center rounded-xl">
+                <ProdutosCategoria />
+
+                <button className="bg-(--primario-700) text-(--terciario-400) gap-3 py-3 text-lg px-1.5 flex justify-center rounded-xl cursor-pointer">
                     <img className="w-6" src={addCategoria} alt="" />
-                    <p>Adicionar categoria</p>
+                    <p>Adicionar produto</p>
+                </button>
+            </div>
+
+            <div className="grid border border-(--primario-600) py-2 px-3 rounded-lg">
+                <div className="flex justify-between pb-1.5">
+                    <h2 className="text-2xl">Petisco</h2>
+                    <button><img src={config} alt="" /></button>
+                </div>
+
+                <ProdutosCategoria />
+
+                <button className="bg-(--primario-700) text-(--terciario-400) gap-3 py-3 text-lg px-1.5 flex justify-center rounded-xl cursor-pointer">
+                    <img className="w-6" src={addCategoria} alt="" />
+                    <p>Adicionar produto</p>
+                </button>
+            </div>
+
+            <div className="grid border border-(--primario-600) py-2 px-3 rounded-lg">
+                <div className="flex justify-between pb-1.5">
+                    <h2 className="text-2xl">Almoço</h2>
+                    <button><img src={config} alt="" /></button>
+                </div>
+
+                <ProdutosCategoria />
+
+                <button className="bg-(--primario-700) text-(--terciario-400) gap-3 py-3 text-lg px-1.5 flex justify-center rounded-xl cursor-pointer">
+                    <img className="w-6" src={addCategoria} alt="" />
+                    <p>Adicionar produto</p>
                 </button>
             </div>
         </section>
