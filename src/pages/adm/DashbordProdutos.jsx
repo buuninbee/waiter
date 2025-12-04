f7817552ba7dc3d115efbe24276a1525f7f614a7
buuninbee <buunin.bee@gmail.com>
2025-12-03 23:04:35 -0300

    feat: alterando o nome do eixo x para nomes de categorias

--- src/pages/adm/DashbordProdutos.jsx
+++ src/pages/adm/DashbordProdutos.jsx
@@ -71,37 +71,37 @@ export default function Dashboard() {
   
   const dataCategoria = [
     {
-      name: 'Page A',
+      name: 'Lanches',
       uv: 590,
       pv: 800,
       amt: 1400,
     },
     {
-      name: 'Page B',
+      name: 'Bebidas',
       uv: 868,
       pv: 967,
       amt: 1506,
     },
     {
-      name: 'Page C',
+      name: 'Combos',
       uv: 1397,
       pv: 1098,
       amt: 989,
     },
     {
-      name: 'Page D',
+      name: 'Porções',
       uv: 1480,
       pv: 1200,
       amt: 1228,
     },
     {
-      name: 'Page E',
+      name: 'Aperitivos',
       uv: 1520,
       pv: 1108,
       amt: 1100,
     },
     {
-      name: 'Page F',
+      name: 'Sobremesas',
       uv: 1400,
       pv: 680,
       amt: 1700,
