cf09e7514c1de097ec660a44b69bd377d8cfc6a5
buuninbee <buunin.bee@gmail.com>
2025-12-03 22:57:10 -0300

    feat: adicionando a função de buscar produtos

--- src/componentes/Lupa.jsx
+++ src/componentes/Lupa.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import IconLupa from "@/assets/lupa.svg"
 import fetchProdutos from '@/api/fetchProdutos'
+import { useContext } from 'react'
+import CardapioContext from '@/context/CardapioContext'
 
 const Lupa = ({placeholder}) => {
 
-  const [handlevalue, setHandleValue] = React.useState('')
+  const { setLoading , setProdutos} = useContext(CardapioContext)
+  const [valorBusca, setValorBusca] = React.useState('')
 
-  const handleSearch = async (event) => {
+  const PesquisaProdutos = async (event) => {
     event.preventDefault()
+    setLoading(true)
 
-    const produto = await fetchProdutos(handlevalue)
-    setHandleValue(produto)
-    setHandleValue()
-
+    const produto = await fetchProdutos(valorBusca)
+    setProdutos(produto)
+    setLoading(false)
+    setValorBusca()
   }
+  // colocar o PesquisarProdutos no onsubmit
   return (
     <form className='flex gap-2 px-2 py-1.5  items-center border rounded-lg'>
         <img className='' src={IconLupa} alt="" />
         <input className='bg-[--terciario-400] w-full text-base appearance-none focus:outline-0' type="search" 
-        value={handlevalue} 
+        value={valorBusca} 
         placeholder={placeholder}
-        onChange={({target}) => setHandleValue(target.value)}
+        onChange={({target}) => setValorBusca(target.value)}
         name="" id=""/>
     </form>
   )
