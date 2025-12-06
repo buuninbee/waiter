4321e8d44cd6cf1cde91168742de63ab1487379d
buuninbee <buunin.bee@gmail.com>
2025-12-06 19:09:58 -0300

    feat: atualizando a logica de carrinho de compra

--- src/componentes/CarrinhoCompra.jsx
+++ src/componentes/CarrinhoCompra.jsx
@@ -5,22 +5,22 @@ import CardapioContext from '@/context/CardapioContext';
 import  {useState, useContext} from "react"
 
 import seta from "@/assets/seta.svg"
-import Botao from './Botao';
 
 function CarrinhoCompra() {
   const {cartItens} = useContext(CardapioContext)
 
-  const qtdproduto = [...cartItens].length
+  const precos = cartItens.map((produto) => produto.preco)
 
-  const totalPreco = cartItens.reduce((acc, item) => item.preco * acc, 0).toFixed(2)
+  const totalPreco = precos.reduce((acc, item) => (item + acc), 0).toFixed(2)
   const [aberto, setAberto] = useState(false)
+
   return (
     <div className='bg-(--primario-800) cursor-pointer px-7'>
       <div  onClick={() => setAberto(!aberto)}  className='flex items-center justify-between'>
         <div className='flex items-center gap-4 text-(--terciario-300) py-4 rounded-t-lg'>
           <div className='relative'>
               <img src={iconSacola} alt="" />
-              <p className='bg-(--terciario-300) text-sm text-(--primario-800) rounded-full px-2 absolute left-4 border border-(--primario-800) bottom-4'>{qtdproduto}</p>
+              <p className='bg-(--terciario-300) text-sm text-(--primario-800) rounded-full px-2 absolute left-4 border border-(--primario-800) bottom-4'>{cartItens.length}</p>
           </div>
           <h3>Faça seu pedido</h3>
          </div>
@@ -42,10 +42,12 @@ function CarrinhoCompra() {
 
          </div>
       </div>
-    <div className={aberto ? "grid" : "hidden"}>
-        <CarrinhoItem/>
+    <div className={aberto ? "grid" : "hidden "}>
+      {
+        cartItens.length > 0 ? <CarrinhoItem/> : <p className='text-center text-xl py-4 text-(--terciario-200)'>Seu carrinho está vazio, adicione um produto</p>
+      }
 
-        <div>
+        <div className='pb-8'>
           <div className='flex text-(--terciario-50) justify-between py-4'>
             <p>Resumo do carrinho</p>
             <span>R$ {totalPreco}</span>
